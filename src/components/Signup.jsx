45251ac4121ac90f4aad5d7aa20cf148db37016f
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-13 23:46:00 +0200

    Update Signup button styling

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -4,8 +4,8 @@ import { Icon } from '@iconify/react';
 export function Signup(){
     return (
         <button
-            className="text-[#3B41C5] bg-white rounded-lg flex items-start p-2 hover:scale-105 duration-150 m-auto">
-                <span><Icon icon="logos:google-gmail" /></span>
+            className="text-[#3B41C5] bg-white rounded-lg flex items-start p-2 hover:scale-105 duration-150 m-auto px-6">
+                <span className="my-auto mx-2"><Icon icon="logos:google-gmail" /></span>
                 <p>Continue with Google</p>
         </button>
     )
