cdd1af372809ca9aec0ae6ac20196e4ad7036903
Mahmoud Galal <ssss.aaa2002@gmail.com>
2023-03-13 23:40:53 +0200

    Update tailwind.config.js to add container with center and padding options

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
   },
   plugins: [],
 }
